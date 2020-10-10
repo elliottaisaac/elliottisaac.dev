3d164e9182f0f04ef7ae627a75c359d957d29409
Elliott Isaac <eisaac22@gmail.com>
2020-10-10 02:54:36 -0400

    no message

--- assets/js/ellibrary.js
+++ assets/js/ellibrary.js
@@ -10,36 +10,48 @@ function qsA(selector){
     return querySelectorAll;
 }
  
-function AEL(selector, event, Function){
-    qs(selector).addEventListener(event, (e) => {
-        Function();
-    });
+function AEL(element, event, Function){
+    if(typeof element === 'string' || element instanceof String){
+        qs(selector).addEventListener(event, (e) => {
+            Function();
+        });
+    }
+    else{
+        element.addEventListener(event, (e) => {
+            Function();
+        });
+    }
 }
 
-function NewElement(element, classes, parent){
-    let newElement = document.createElement(element);
+function NewElement(type, classes, parent){
+    let newElement = document.createElement(type);
     for(i = 0; i < classes.length; i++){
         newElement.classList.add(classes[i]);
     }
-    qs(parent).appendChild(newElement);
+    if(typeof parent === 'string' || parent instanceof String) qs(parent).appendChild(newElement);
+    else parent.appendChild(newElement);
 }
 
 function log(x){
     console.log(x);
 }
 
-function style(selector, styles){
-    qs(selector).style = `${styles}`;
+function style(element, styles){
+    if(typeof element === 'string' || element instanceof String) qs(selector).style = `${styles}`;
+    else element.style = `${styles}`;
 }
 
-function AddClass(selector, Class){
-    qs(selector).classList.add(`.${Class}`);
+function AddClass(element, Class){
+    if(typeof element === 'string' || element instanceof String) qs(element).classList.add(`.${Class}`);
+    else element.classList.add(`.${Class}`);
 }
 
-function RemoveClass(selector, Class){
-    qs(selector).classList.remove(`.${Class}`);
+function RemoveClass(element, Class){
+    if(typeof element === 'string' || element instanceof String) qs(element).classList.remove(`.${Class}`);
+    else element.classList.remove(`.${Class}`);
 }
 
-function SetAttribute(selector, attribute, value){
-    qs(selector).setAttribute(`${attribute}`, value);
+function SetAttribute(element, attribute, value){
+    if(typeof element === 'string' || element instanceof String) qs(element).setAttribute(`${attribute}`, value);
+    else element.setAttribute(`${attribute}`, value);
 }
