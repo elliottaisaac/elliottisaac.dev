d482a1110c8a4b09b06b04a9724220cced90cc60
Elliott Isaac <eisaac22@gmail.com>
2020-10-10 02:20:42 -0400

    no message

--- assets/js/ellibrary.js
+++ assets/js/ellibrary.js
@@ -9,22 +9,7 @@ function qsA(selector){
     let querySelectorAll = document.querySelectorAll(selector);
     return querySelectorAll;
 }
-
-function qsaC(Class){
-   let querySelectorAllFromClass = (`.${Class}`);
-   return querySelectorAllFromClass;
-}
-
-function qsE(element){
-    let querySelectorElement = document.querySelector(`#${element}`);
-    return querySelectorElement;
- }
  
-function qsID(id){
-   let querySelectorID = document.querySelector(`#${id.id}`);
-   return querySelectorID;
-}
-
 function AEL(selector, event, Function){
     qs(selector).addEventListener(event, (e) => {
         Function();
@@ -58,4 +43,3 @@ function RemoveClass(selector, Class){
 function SetAttribute(selector, attribute, value){
     qs(selector).setAttribute(`${attribute}`, value);
 }
-
