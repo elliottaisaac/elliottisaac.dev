731a30bbd4c7618cde2b70b6ca38167efbc0564f
Elliott Isaac <eisaac22@gmail.com>
2021-09-21 15:29:56 -0400

    ellibrary

--- tools/ellibrary.js
+++ tools/ellibrary.js
@@ -1,26 +1,31 @@
 // Elliott's JS Library!
 
-//Write to Console
+
+// Write to Console
 function log(x){
     console.log(x);
 }
 
-//Query Selector
+
+// Query Selector
 function qs(selector){
     return document.querySelector(selector);
  }
 
-//Query Selector All
+
+// Query Selector All
 function qsa(selector){
     return document.querySelectorAll(selector);
 }
 
-//Query Selector All - return array instead of node list
+
+// Query Selector All - return array instead of node list
 function qsarray(selector){
     return Array.from(document.querySelectorAll(selector));
 }
  
-//Add Event Listener - element parameter can be selector string or node object
+
+// Add Event Listener - element parameter can be selector string or node object
 function ael(element, event, Function){
     if(typeof element === 'string' || element instanceof String){
         qs(element).addEventListener(event, (e) => {
@@ -34,30 +39,41 @@ function ael(element, event, Function){
     }
 }
 
+
+// Adds and/or removes CSS classes
+// Takes a node object/element and an array of strings as inputs
+// Assigns element a the CSS class named in each value of the array...
+// UNLESS the string starts with '!' in which case it removes the class whose name follows
+function UpdateClasses(element, classNameArray){
+    for(let i = 0; i < classNameArray.length; i++){
+        if(classNameArray[i].charAt(0) != "!") element.classList.add(classNameArray[i]);
+        else element.classList.remove(classNameArray[i].substring(1));
+    }
+}
+
+
 //Insert element after existing element
 function InsertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
-//Create New Element
-//- classes param accepts array containing class names, if no classes use false. 
-//- use false for referenceElement param to return element as node object
-//- use a node object for referenceElement param to add new element to document after reference element
+
+// Create new DOM element
+// Classes param accepts array containing class names, if no classes use false. 
+// Use false for referenceElement param to return element as node object
+// Use a node object for referenceElement param to add new element to document after reference element
 function NewElement(type, classes, referenceElement){
-    let newElement = document.createElement(type);
-    if(classes){
-        for(let i = 0; i < classes.length; i++){
-            newElement.classList.add(classes[i]);
-        }
-    }
+    const newElement = document.createElement(type);
+    newElement(UpdateClasses(classes));
     if(referenceElement) InsertAfter(newElement, referenceElement);
-    else return newElement;
+    return newElement;
 }
 
-//Get size and position of element
-function MeasureElement(element){
-    let rect = element.getBoundingClientRect();
-    let dimensions = {
+
+// Get size and position of element
+function GetDimensions(element){
+    const rect = element.getBoundingClientRect();
+    const dims = {
         top: rect.top,
         left: rect.left,
         bottom: rect.bottom,
@@ -65,30 +81,33 @@ function MeasureElement(element){
         width: rect.width,
         height: rect.height
     }
-    return dimensions;
+    return dims;
 }
 
-//Get width and height of viewport
+
+// Get width and height of viewport
 function MeasureViewport(){
-    let viewportDimensions = {
+    const viewportDimensions = {
         width: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
         height: Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
     }
     return viewportDimensions;
 }
 
-//Get RBG color values from hex code
-function ConvertHexToRGB(hex) {
-    let c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    const red = parseInt(c[1], 16);
-    const green = parseInt(c[2], 16);
-    const blue = parseInt(c[3], 16);
-    return {
-        r: red,
-        g: green,
-        b: blue,
-        rbg: `rgb(${red}, ${green}, ${blue})`
-    };
-}
-    
 
+// Returns object that checks viewport size against am array containing reponsiveness breakpoints for mobile, tablet, and desktop 
+// input false in place of array to use default breakpoints (mobile: 768px, tablet: 992px, desktop: 1200px)
+function CheckViewportResponsiveness(breakpoints){
+    if(!breakpoints) breakpoints = [768, 992, 1200];
+    let breakpointChecks = {
+        isMobile: false,
+        isTablet: false,
+        isDesktop: false
+    }
+    const viewportWidth = MeasureViewport().width;
+    if(viewportWidth < breakpoints[0]) breakpointChecks.isMobile = true;
+    else if(viewportWidth < breakpoints[1]) breakpointChecks.isTablet = true;
+    else  breakpointChecks.isDesktop = true;
+    return breakpointChecks;
+}
+function CVR(){ return CheckViewportResponsiveness(false); } // shorthand for using defaults
