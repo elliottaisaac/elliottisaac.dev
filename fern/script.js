f6c3ad941848723c1daeef95ca1c57d4e03091af
elliottaisaac <49501453+elliottaisaac@users.noreply.github.com>
2024-05-14 15:05:37 -0400

    save fern as image

--- fern/script.js
+++ fern/script.js
@@ -108,3 +108,16 @@ function Draw(){
 		}, 1);	
 	}
 }
+
+// Save | Download image
+document.getElementById('save').addEventListener("click", function(e) {
+    const dataURL = canvasElement.toDataURL("image/png", 1.0);
+    downloadImage(dataURL, 'fractal-fern.png');
+});
+function downloadImage(data, filename = 'untitled.jpeg') {
+    const a = document.createElement('a');
+    a.href = data;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+}
