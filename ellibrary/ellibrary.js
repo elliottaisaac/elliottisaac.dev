389620a0cb7fbac38d4d32e5e3a9dcc33d859bb8
elliottaisaac <49501453+elliottaisaac@users.noreply.github.com>
2024-07-23 15:56:49 -0400

    ellibrary demo page adjustmnts

--- ellibrary/ellibrary.js
+++ ellibrary/ellibrary.js
@@ -2,30 +2,43 @@
 
 
 // Write to Console
+
 function log(x){
     console.log(x);
 }
 
 
+
+
 // Query Selector
+
 function qs(selector){
     return document.querySelector(selector);
  }
 
 
+
+
 // Query Selector All
+
 function qsa(selector){
     return document.querySelectorAll(selector);
 }
 
 
+
+
 // Query Selector All - return array instead of node list
+
 function qsarray(selector){
     return Array.from(document.querySelectorAll(selector));
 }
  
 
+
+
 // Add Event Listener - element parameter can be selector string or node object
+
 function ael(element, event, Function){
     if(typeof element === 'string' || element instanceof String){
         qs(element).addEventListener(event, (e) => {
@@ -40,10 +53,13 @@ function ael(element, event, Function){
 }
 
 
+
+
 // Adds and/or removes CSS classes
 // Takes a node object/element and an array of strings as inputs
 // Assigns element a the CSS class named in each value of the array...
 // UNLESS the string starts with '!' in which case it removes the class whose name follows
+
 function UpdateClasses(element, classNameArray){
     if(classNameArray){
         for(let i = 0; i < classNameArray.length; i++){
@@ -54,16 +70,22 @@ function UpdateClasses(element, classNameArray){
 }
 
 
+
+
 //Insert element after existing element
+
 function InsertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
+
+
 
 
 // Create new DOM element
 // Classes param accepts array containing class names, if no classes use false. 
 // Use false for referenceElement param to return element as node object
 // Use a node object for referenceElement param to add new element to document after reference element
+
 function NewElement(type, classes, referenceElement){
     const newElement = document.createElement(type);
     UpdateClasses(newElement, classes);
@@ -72,7 +94,10 @@ function NewElement(type, classes, referenceElement){
 }
 
 
+
+
 // Get size and position of element
+
 function GetDimensions(element){
     const rect = element.getBoundingClientRect();
     const dims = {
@@ -87,7 +112,10 @@ function GetDimensions(element){
 }
 
 
+
+
 // Get width and height of viewport
+
 function MeasureViewport(){
     const viewportDimensions = {
         width: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
@@ -97,8 +125,11 @@ function MeasureViewport(){
 }
 
 
+
+
 // Returns object that checks viewport size against am array containing reponsiveness breakpoints for mobile, tablet, and desktop 
 // input false in place of array to use default breakpoints (mobile: 768px, tablet: 992px, desktop: 1200px)
+
 function CheckViewportResponsiveness(breakpoints){
     if(!breakpoints) breakpoints = [768, 992, 1200];
     let breakpointChecks = {
@@ -114,7 +145,11 @@ function CheckViewportResponsiveness(breakpoints){
 }
 function CVR(){ return CheckViewportResponsiveness(false); } // shorthand for using defaults
 
+
+
+
 //convert int to binary string
+
 function toBinary(x){
     let binary = "";
     for(let i = 0; x/Math.pow(2,i) >= 1; i++){
